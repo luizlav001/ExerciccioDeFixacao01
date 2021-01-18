12816c1304e271b8a64ad63f4cd42f7d761e3749
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:09:29 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciccioDeFixacao01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciccioDeFixacao01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo p = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            p.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaP = p.Area();
+            double perimetroP = p.Perimetro();
+            double diagonalP = p.Diagonal();
+
+            Console.WriteLine("AREA = " + areaP.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO  = " + perimetroP.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonalP.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ ExerciccioDeFixacao01/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciccioDeFixacao01
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+
+    }
+
+
+}
